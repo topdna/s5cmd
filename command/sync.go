5603653f8928ad9bc53ec5d0665d85a5751494aa
GRMrGecko <grmrgecko@gmail.com>
2024-02-07 13:57:05 -0600

    Add max delete option

--- command/sync.go
+++ command/sync.go
@@ -80,6 +80,11 @@ func NewSyncCommandFlags() []cli.Flag {
 			Name:  "delete",
 			Usage: "delete objects in destination but not in source",
 		},
+		&cli.IntFlag{
+			Name:  "max-delete",
+			Usage: "don't delete more than NUM files",
+			Value: -1,
+		},
 		&cli.BoolFlag{
 			Name:  "size-only",
 			Usage: "make size of object only criteria to decide whether an object should be synced",
@@ -136,6 +141,7 @@ type Sync struct {
 
 	// flags
 	delete      bool
+	maxDelete   int
 	sizeOnly    bool
 	hashOnly    bool
 	exitOnError bool
@@ -162,6 +168,7 @@ func NewSync(c *cli.Context) Sync {
 
 		// flags
 		delete:      c.Bool("delete"),
+		maxDelete:   c.Int("max-delete"),
 		sizeOnly:    c.Bool("size-only"),
 		hashOnly:    c.Bool("hash-only"),
 		exitOnError: c.Bool("exit-on-error"),
@@ -524,6 +531,10 @@ func (s Sync) planRun(
 			}
 
 			if len(dstURLs) == 0 {
+				return
+			}
+			if len(dstURLs) >= s.maxDelete && s.maxDelete >= 0 {
+				fmt.Printf("Not deleting due %d being higher than maximum delete limit\n", len(dstURLs))
 				return
 			}
 
